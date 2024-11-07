3c08870958586d397aae59d8e8c38c5457745b29
知道 不 <s95510691@gmail.com>
2024-11-07 17:37:29 +0800

    file share part function test22

--- WirelessTransfer/CustomControls/FileShareProgressTag.xaml.cs
+++ WirelessTransfer/CustomControls/FileShareProgressTag.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net.Sockets;
@@ -161,6 +162,19 @@ namespace WirelessTransfer.CustomControls
                     completeGrid.Visibility = Visibility.Visible;
                     break;
             }
+        }
+    }
+
+    public class MyCustomConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new Thickness(0, 0, -(double)value, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
