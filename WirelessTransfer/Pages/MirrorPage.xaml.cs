299a4d8f358066aa81f5e2fe798e38114098227e
知道 不 <s95510691@gmail.com>
2024-11-09 15:41:03 +0800

    Revert "udp mirror test3"
    This reverts commit 2571cef4d1cfe71dd47905a0322fe7ab5034d696.

--- WirelessTransfer/Pages/MirrorPage.xaml.cs
+++ WirelessTransfer/Pages/MirrorPage.xaml.cs
@@ -161,9 +161,7 @@ namespace WirelessTransfer.Pages
                 }
             }
 
-            screenUdpClient = new UdpClient();
-            screenUdpClient.Client.SendBufferSize = 6291456; // 6MB
-            screenUdpClient.Client.ReceiveBufferSize = 6291456; // 6MB
+            screenUdpClient = new UdpClient(new IPEndPoint(myTcpServer.ConnectedClients.First().Address, udpPort));
 
             screenCaptureDX = new ScreenCaptureDX(0);
             screenCaptureDX.ScreenRefreshed += screenCaptureDX_ScreenRefreshed;
@@ -177,8 +175,7 @@ namespace WirelessTransfer.Pages
                 try
                 {
                     //myTcpServer.SendCmd(new ScreenCmd(e), myTcpServer.ConnectedClients.First());
-                    screenUdpClient.Send(new ScreenCmd(e).Encode(),
-                        new IPEndPoint(myTcpServer.ConnectedClients.First().Address, udpPort));
+                    screenUdpClient.Send(new ScreenCmd(e).Encode());
                 }
                 catch { }
             }
